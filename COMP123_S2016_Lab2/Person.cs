02f4eb4cb9ff966a075e3a694e9aaa5800bf3858
mirsakiyev <mirsakiyev@yahoo.com>
2016-05-26 15:37:08 -0400

    Added a "Person" class and initial comments

---
+++ COMP123_S2016_Lab2/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lab2
+{
+    /**
+     * This class defines a generic Person
+     *
+     * @class Person
+     * 
+     */
+    class Person
+    {
+    }
+}
---
+++ COMP123_S2016_Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lab2
+{
+    /**
+     * This class is the driver class for our Program
+     * 
+     * @class Program
+     */
+    class Program
+    {
+        /**
+         * The main method for our driver class Program
+         *
+         * @method Main
+         * @param {strinf[]} args
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
