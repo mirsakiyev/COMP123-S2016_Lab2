0ccc3145554a580d575ab6847fc19c0260866a32
mirsakiyev <mirsakiyev@yahoo.com>
2016-05-26 15:43:06 -0400

    Added private instance variables : _name, _age; and public property : Name.

--- COMP123_S2016_Lab2/Person.cs
+++ COMP123_S2016_Lab2/Person.cs
@@ -14,5 +14,19 @@ namespace COMP123_S2016_Lab2
      */
     class Person
     {
+        // PRIVATE INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private string _name;
+        private int _age;
+
+        // PUBLIC PROPERTy ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        /**
+         * This is a property for our _name field
+         * @property {string} Name
+         */
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
     }
 }
